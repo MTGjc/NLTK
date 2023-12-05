5d92a08fe5fd60c796bde69cf0bc00d973d432cd
MTGjc <zhou.junchen.zjc@gmail.com>
2023-12-05 16:19:01 +0000

    extract text from video input
    Video File ("Sequence 01.mp4")can be changed to any

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NLTK
  prjct_tool0
+
+ElevenLabs(sst)api: 3e43a4dc8926e94d0d19b5bedc743e5a
+Chatgpt api: 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
--- textRecog.py
+++ textRecog.py
@@ -1,3 +1,4 @@
+import socket
 from PIL import Image
 import pytesseract
 from wand.image import Image as Img
@@ -13,52 +14,55 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 #setup folder to store images
 image_frames = 'image_frames'
-print(pytesseract.image_to_string(Image.open('nltk0.png')))
+# testimage = pytesseract.image_to_string(Image.open('nltk0.png'))
+# print(testimage)
 
-# def files():
+def files():
 
-#     try:
-#         os.remove(image_frames)
-#     except OSError:
-#         pass
+    try:
+        os.remove(image_frames)
+    except OSError:
+        pass
 
-#     if not os.path.exists(image_frames):
-#         os.makedirs(image_frames)
+    if not os.path.exists(image_frames):
+        os.makedirs(image_frames)
     
-#     src_vid = cv2.VideoCapture('Who_Am_I_JuntongZhou')
-#     return(src_vid)
+    # specify source video path
+    src_vid = cv2.VideoCapture('Sequence 01.mp4')
+    return(src_vid)
 
-# def process(src_vid):
+def process(src_vid):
     
-#     #name file
-#     index =0
-#     while src_vid.isOpened():
-#         ret, frame = src_vid.read()
-#         if not ret:
-#             break
+    # name file
+    index =0
+    while src_vid.isOpened():
+        ret, frame = src_vid.read()
+        if not ret:
+            break
 
-#         name = './image_frames/frame'+ str(index) + '.png'
+        name = './image_frames/frame'+ str(index) + '.png'
 
-#         #save every 150 frame
-#         if index % 150 ==0:
-#             print ('Extract frame...'+ name)
-#             cv2.imwrite(name, frame)
-#         index = index +1
-#         if cv2.waitKey(10) & 0xFF == ord('q'):
-#             break
+        #save every 150 frame
+        if index % 200 ==0:
+            print ('Extract frame...'+ name)
+            cv2.imwrite(name, frame)
+        index = index +1
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
-#     src_vid.realise()
-#     cv2.destroyAllWindows()
+    src_vid.realise()
+    cv2.destroyAllWindows()
 
 # #image to text on each png
 
-# def get_text():
-#     for i in os.listdir(image_frames):
-#         my_example = Image.open(image_frames + "/" + i)
-#         text = pytesseract.image_to_string(my_example, lang='en')
+def get_text():
+    for i in os.listdir(image_frames):
+        my_example = Image.open(image_frames + "/" + i)
+        text = pytesseract.image_to_string(my_example, lang='eng+chi_sim')
+        print (text)
 
 # #main drive
-# if __name__ == '__main__':
-#     vid = files()
-#     process(vid)
-#     get_text()
+if __name__ == '__main__':
+    vid = files()
+    process(vid)
+    get_text()
